6bf416df531ed2bbecaaf27600a5ca17c17bd834
Morella92 <morena.piem@outlook.com>
2023-03-30 13:09:30 +0200

    START created class with variables and functions

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+    include './movie.php';    
+
+    $movie = new Movie("Kill Bill", "2003");
+
+    var_dump($movie)
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP</title>
+</head>
+<body>
+    
+
+</body>
+</html>
---
+++ movie.php
@@ -0,0 +1,24 @@
+<?php
+
+// Dichiaro la classe
+
+class Movie {
+
+  // Dichiarazione delle variabili d'istanza
+  public $title;
+  public $director;
+  public int $year;
+  public $genre;
+
+  // Definizione del costruttore
+    function __construct($_title, $_year) {
+        $this->title = $_title;
+        $this->year = $_year; 
+    }
+
+  function getTitle(){
+    return $this->title;
+  }
+}
+
+?>
